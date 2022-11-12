d170e8f4694db27d14b451e3cefd3c957f20d81f
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-11-12 23:57:43 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -2,8 +2,8 @@
 #define _SORT_H_
 #include studio.h
 
-void print_list(const listint_t *list):
-void print_array(const int *array, size_t size):
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
